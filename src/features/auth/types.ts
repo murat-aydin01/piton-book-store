4c7fff301b7ac410f861b82eaefb3f1f9b518f6e
murat-aydin01 <murataydin2001@outlook.com>
2025-03-24 21:47:13 +0300

    feature

---
+++ src/features/auth/types.ts
@@ -0,0 +1,12 @@
+export type LoginFormType = {
+  email: string;
+  password: string;
+};
+
+export type RegisterFormType = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+//token type oluştur
