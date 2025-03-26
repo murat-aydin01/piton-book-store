a65d4dd15b4320aa45db016e5f30fb57da2efbb4
Murat AYDIN <murataydin2001@outlook.com>
2025-03-26 09:27:12 +0300

    buton auth oluşturuldu

---
+++ src/features/auth/components/ButtonAuth.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary";
+};
+function ButtonAuth({ children, variant = "primary" }: Props) {
+  const baseClasses = "px-40 py-3 w-full font-semibold rounded-sm text-2xl";
+  const primaryClasses = "text-white bg-[#EF6B4A]";
+  const secondaryClasses = "text-[#6251DD] border border-[#6251DD]";
+  return <button className={`${baseClasses} ${variant==="primary"||primaryClasses} ${variant==="secondary"||secondaryClasses}`}>{children}</button>;
+}
+
+export default ButtonAuth;
