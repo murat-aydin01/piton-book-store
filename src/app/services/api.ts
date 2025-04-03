a87b4d66a26005d646730d4cd3a17ad6738fdd36
Murat AYDIN <murataydin2001@outlook.com>
2025-04-03 17:52:07 +0300

    api interceptor güncelleme

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,20 +1,30 @@
 import axios from "axios";
 import { LoginSchema, RegisterSchema } from "../(auth)/utils/authSchema";
+import { getToken, removeToken } from "../(auth)/utils/authLocalStorage";
 
-const authInstance = axios.create({baseURL: "https://assign-api.piton.com.tr/api/rest"});
+/* 
+TODO api cevabı için tip belirleme nasıl yapılır
+type LoginResponse = {
+  action_login: {
+    token: string
+  }
+}
+type RegisterResponse = {
+  action_register: {
+    token: string
+  }
+} */
+
 const apiInstance = axios.create({baseURL: "https://assign-api.piton.com.tr/api/rest"});
-// TODO token kontrolü yapılacak
 apiInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    if (!config.url?.includes('/login') && !config.url?.includes('/register')) {
+      const token = getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
   }
 );
 
@@ -22,39 +32,13 @@ apiInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
+      removeToken()
       window.location.href = "/login";
     }
     return Promise.reject(error);
   }
 );
 
-export const fetcher = (url: string) =>
-  authInstance.get(url).then((res) => res.data);
-
-const authRequest = async (
-  endpoint: string,
-  data: LoginSchema | RegisterSchema
-) => {
-  try {
-    const response = await authInstance.post(endpoint, data);
-    const token = response.data?.[`action_${endpoint.replace("/", "")}`]?.token;
-
-    if (token) {
-      localStorage.setItem("token", token);
-      window.location.href = "/"
-    } else {
-      throw new Error("Token alınamadı");
-    }
-
-    return response.data;
-  } catch (error) {
-    console.error(`Hata: ${error}`);
-    throw error;
-  }
-};
-
-export const login = (data: LoginSchema) => authRequest("/login", data);
-
-export const register = (data: RegisterSchema) =>
-  authRequest("/register", data);
+export const login = (data: LoginSchema) => apiInstance.post("/login", data);
+export const register = (data: RegisterSchema) => apiInstance.post("/register", data);
+export const fetcher = (url: string) => apiInstance.get(url).then((res) => res.data);
